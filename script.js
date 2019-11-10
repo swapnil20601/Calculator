88705f452303f3359ab22aa848bca728aaa36e90
Swapnil <t283500@CORP.TCF.BIZ>
2019-11-10 10:11:27 -0600

    trying to listen to operator event

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 const screen = document.getElementById("screen");
 
-let firstOperand = null;
+let firstOperand = '';
+
+let secondOperand = '';
 
 let numberButtonElements = document.querySelectorAll("button[data-number]");
+
+let operator = document.querySelectorAll("button[data-operator]")
+
+//returns true if operator is clicked
+let isOperator = operator.forEach(op => op.addEventListener('click', function() {
+  debugger
+  return true;
+}));
+
+
 
 numberButtonElements.forEach(element =>
   element.addEventListener("click", function(e) {
       const btn = e.target.textContent;
-      if (screen.value === "0") {
-        screen.value = "";
-        firstOperand = screen.value + btn;
-        screen.value = firstOperand;
-        console.log(firstOperand)
-      } else if (screen.value.length < 10) {
-        firstOperand = screen.value + btn;
-        screen.value = firstOperand;
-        console.log(firstOperand)
-      } else {
-        alert("Only 10 digits are allowed!");
+      console.log(btn);
+
+      //if isOperator is true, then clear screen and display secondOperand, ELSE, display firstOperand on screen
+      if(isOperator) {
+        console.log('operation is true')
+        screen.value = '';
+        secondOperand = secondOperand + btn;
+        screen.value = secondOperand;
       }
-       
+       else {
+        console.log('false operator')
+        if (screen.value === "0") {
+          firstOperand = firstOperand +  btn;
+          screen.value = firstOperand
+        } else if (screen.value.length < 10) {
+          firstOperand = firstOperand +  btn;
+          screen.value = firstOperand
+        } else {
+          alert("Only 10 digits are allowed!");
+        } 
+      }
+
   })
 );
 
-console.log(firstOperand)
+document.getElementById('x').addEventListener('click', function(e){
+  console.log(isOperator)
+})
